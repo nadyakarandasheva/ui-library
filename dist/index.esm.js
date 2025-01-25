3bf304cdfd362ddb093ac6a209df5444a13c70e5
Nadya-Karandasheva <nadya.karandasheva.2000@mail.ru>
2025-01-25 17:03:44 +0300

    fix

--- dist/index.esm.js
+++
@@ -1,9 +0,0 @@
-import { jsx } from 'react/jsx-runtime';
-
-var Button = function (_a) {
-    var label = _a.label, onClick = _a.onClick;
-    return jsx("button", { style: { background: "#dad" }, onClick: onClick, children: label });
-};
-
-export { Button };
-//# sourceMappingURL=index.esm.js.map
