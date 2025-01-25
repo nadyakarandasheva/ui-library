a9185079c8356136453abcedcc8b1b8d19791050
Nadya-Karandasheva <nadya.karandasheva.2000@mail.ru>
2025-01-25 17:03:25 +0300

    fix

--- dist/components/atoms/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface ButtonProps {
-    label: string;
-    onClick: () => void;
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from "./components/atoms/Button/Button";
+export * from "./components";
