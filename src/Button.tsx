560c29552f791e9c22c1dd7026ea2eee88f06107
Nadya-Karandasheva <nadya.karandasheva.2000@mail.ru>
2025-01-26 16:55:35 +0300

    change button color

--- src/Button.tsx
+++ src/Button.tsx
@@ -6,5 +6,5 @@ interface ButtonProps {
 };
 
 export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button style={{ background: "#fff", padding: '10px 8px', border: '1px solid #000', borderRadius: '25px' }} onClick={onClick}>{label}</button>;
+  return <button style={{ background: "#b3baf286", padding: '10px 8px', border: '1px solid #000', borderRadius: '25px' }} onClick={onClick}>{label}</button>;
 }; 
